9cdc8c2cbc940b824b3cccede545fe419b668e69
mumichae <mi.mueller@tum.de>
2020-11-09 00:21:55 +1100

    removed get_filename_pattern function and fixed scripts paths

--- Snakefile
+++ Snakefile
@@ -5,164 +5,17 @@ cfg = ParsedConfig(config)
 wildcard_constraints:
     hvg = "hvg|full_feature"
 
+include: "scripts/preprocessing/Snakefile"
 include: "scripts/integration/Snakefile"
-#include: "scripts/metrics/Snakefile"
+include: "scripts/metrics/Snakefile"
+include: "scripts/visualization/Snakefile"
+include: "scripts/metrics/cc_score/Snakefile"
 
 rule all:
     input:
-        cfg.get_filename_pattern("metrics", "final"),
-        cfg.get_filename_pattern("embeddings", "final")
-
-# ------------------------------------------------------------------------------
-# Compute metrics
-# ------------------------------------------------------------------------------
-
-rule metrics_unintegrated:
-    input:        cfg.get_all_file_patterns("metrics_unintegrated")
-    message:        "Collect all unintegrated metrics"
-
-rule metrics_integrated:
-    input: cfg.get_all_file_patterns("metrics")
-    message: "Collect all integrated metrics"
-
-all_metrics = rules.metrics_integrated.input
-if cfg.unintegrated_m:
-    all_metrics.extend(rules.metrics_unintegrated.input)
-
-rule metrics:
-    input:
-        tables = all_metrics,
-        script = "scripts/merge_metrics.py"
-    output:
-        cfg.get_filename_pattern("metrics", "final")
-    message: "Merge all metrics"
-    params:
-        cmd = f"conda run -n {cfg.py_env} python"
-    shell: "{params.cmd} {input.script} -i {input.tables} -o {output} --root {cfg.ROOT}"
-
-def get_integrated_for_metrics(wildcards):
-    if wildcards.method == "unintegrated":
-        pattern = str(rules.integration_prepare.output)
-        file = os.path.splitext(pattern)[0]
-        return f"{file}.h5ad"
-    elif cfg.get_from_method(wildcards.method, "R"):
-        return cfg.get_filename_pattern("integration", "single", "rds_to_h5ad")
-    else:
-        return cfg.get_filename_pattern("integration", "single", "h5ad")
-
-rule metrics_single:
-    input:
-        u      = lambda wildcards: cfg.get_from_scenario(wildcards.scenario, key="file"),
-        i      = get_integrated_for_metrics,
-        script = "scripts/metrics.py"
-    output: cfg.get_filename_pattern("metrics", "single")
-    message:
-        """
-        Metrics {wildcards}
-        output: {output}
-        """
-    params:
-        batch_key = lambda wildcards: cfg.get_from_scenario(wildcards.scenario, key="batch_key"),
-        label_key = lambda wildcards: cfg.get_from_scenario(wildcards.scenario, key="label_key"),
-        organism  = lambda wildcards: cfg.get_from_scenario(wildcards.scenario, key="organism"),
-        assay     = lambda wildcards: cfg.get_from_scenario(wildcards.scenario, key="assay"),
-        hvgs      = lambda wildcards: cfg.get_feature_selection(wildcards.hvg),
-        cmd       = f"conda run -n {cfg.py_env} python"
-    shell:
-        """
-        {params.cmd} {input.script} -u {input.u} -i {input.i} -o {output} -m {wildcards.method} \
-        -b {params.batch_key} -l {params.label_key} --type {wildcards.o_type} \
-        --hvgs {params.hvgs} --organism {params.organism} --assay {params.assay} -v
-        """
-
-# ------------------------------------------------------------------------------
-# Save embeddings
-# ------------------------------------------------------------------------------
-
-rule embeddings_unintegrated:
-    input:
-        cfg.get_all_file_patterns("embeddings_unintegrated")
-    message:
-        "Collect all unintegrated embeddings"
-
-rule embeddings_integrated:
-    input: cfg.get_all_file_patterns("embeddings")
-    message: "Collect all integrated embeddings"
-
-all_embeddings = rules.embeddings_integrated.input
-if cfg.unintegrated_m:
-    all_embeddings.extend(rules.embeddings_unintegrated.input)
-
-rule embeddings:
-    input:
-        csvs = all_embeddings
-    output:
-        cfg.get_filename_pattern("embeddings", "final")
-    message:
-        "Completed all embeddings"
-    shell:
-     """
-     echo '{input.csvs}' | tr " " "\n" > {output}
-     """
-
-rule embeddings_single:
-    input:
-        adata  = get_integrated_for_metrics,
-        script = "scripts/save_embeddings.py"
-    output:
-        coords = cfg.get_filename_pattern("embeddings", "single"),
-        batch_png = cfg.get_filename_pattern("embeddings", "single").replace(".csv", "_batch.png"),
-        labels_png = cfg.get_filename_pattern("embeddings", "single").replace(".csv", "_labels.png")
-    message:
-        """
-        SAVE EMBEDDING
-        Scenario: {wildcards.scenario} {wildcards.scaling} {wildcards.hvg}
-        Method: {wildcards.method} {wildcards.o_type}
-        Input: {input.adata}
-        Output: {output}
-        """
-    params:
-        batch_key = lambda wildcards: cfg.get_from_scenario(wildcards.scenario, key="batch_key"),
-        label_key = lambda wildcards: cfg.get_from_scenario(wildcards.scenario, key="label_key"),
-        cmd       = f"conda run -n {cfg.py_env} python"
-    shell:
-        """
-        {params.cmd} {input.script} --input {input.adata} --outfile {output.coords} \
-            --method {wildcards.method} --batch_key {params.batch_key} \
-            --label_key {params.label_key} --result {wildcards.o_type}
-        """
-
-# ------------------------------------------------------------------------------
-# Cell cycle score sanity check
-# ------------------------------------------------------------------------------
-
-rule cc_variation:
-    input:
-        tables = cfg.get_all_file_patterns("cc_variance", output_types=["full", "embed"]),
-        script = "scripts/merge_cc_variance.py"
-    output: cfg.get_filename_pattern("cc_variance", "final")
-    params:
-        cmd = f"conda run -n {cfg.py_env} python"
-    shell: "{params.cmd} {input.script} -i {input.tables} -o {output} --root {cfg.ROOT}"
-
-rule cc_single:
-    input:
-        u      = lambda wildcards: cfg.get_from_scenario(wildcards.scenario, key="file"),
-        i      = cfg.get_filename_pattern("integration", "single"),
-        script = "scripts/cell_cycle_variance.py"
-    output: cfg.get_filename_pattern("cc_variance", "single")
-    params:
-        batch_key = lambda wildcards: cfg.get_from_scenario(wildcards.scenario, key="batch_key"),
-        organism  = lambda wildcards: cfg.get_from_scenario(wildcards.scenario, key="organism"),
-        assay     = lambda wildcards: cfg.get_from_scenario(wildcards.scenario, key="assay"),
-        hvgs      = lambda wildcards: cfg.get_feature_selection(wildcards.hvg),
-        cmd       = f"conda run -n {cfg.py_env} python"
-    shell:
-        """
-        {params.cmd} {input.script} -u {input.u} -i {input.i} -o {output} \
-        -b {params.batch_key} --assay {params.assay} --type {wildcards.o_type} \
-        --hvgs {params.hvgs} --organism {params.organism}
-        """
+        rules.integration.input,
+        rules.metrics.output,
+        rules.embeddings.output
 
 # ------------------------------------------------------------------------------
 # Merge benchmark files
@@ -175,7 +28,7 @@ rule benchmarks:
     input:
         script = "scripts/merge_benchmarks.py"
     output:
-        cfg.get_filename_pattern("benchmarks", "final")
+        cfg.ROOT / "benchmarks.csv"
     message: "Merge all benchmarks"
     params:
         cmd = f"conda run -n {cfg.py_env} python"
--- config/ScibConfig.py
+++ config/ScibConfig.py
@@ -1,4 +1,4 @@
-import pathlib
+from pathlib import Path
 from snakemake.io import expand
 from collections import defaultdict
 import itertools
@@ -9,25 +9,20 @@ def as_list(x):
 
 
 def join_path(*args):
-    path = pathlib.Path(args[0])
+    path = Path(args[0])
     for d in args[1:]:
         path = path / d
     return str(path)
 
 
 class ParsedConfig:
-    OUTPUT_FILE_TYPES = ['prepare', 'integration', 'metrics', 'metrics_unintegrated',
-                         'embeddings', 'embeddings_unintegrated', 'cc_variance',
-                         'benchmarks']
-    OUTPUT_LEVELS = ['single', 'final', 'by_method', 'by_method_scaling',
-                     'directory_by_setting']
     OUTPUT_TYPES = ['full', 'embed', 'knn']
 
     def __init__(self, config):
 
         # TODO: define and check schema of config
 
-        self.ROOT = pathlib.Path(config["ROOT"])
+        self.ROOT = Path(config["ROOT"])
         self.DATA_SCENARIOS = config["DATA_SCENARIOS"]
         self.SCALING = config["SCALING"]
         self.FEATURE_SELECTION = config["FEATURE_SELECTION"]
@@ -99,50 +94,6 @@ class ParsedConfig:
         if key not in self.DATA_SCENARIOS[scenario]:
             return False
         return self.DATA_SCENARIOS[scenario][key]
-
-    def get_filename_pattern(self, file_type, level, file_suffix=None):
-        """
-        file_type: ParsedConfig.OUTPUT_FILE_TYPES
-        level: one of ParsedConfig.OUTPUT_LEVELS
-        """
-        if file_type not in ParsedConfig.OUTPUT_FILE_TYPES:
-            raise ValueError(f"{file_type} not a valid output file type")
-
-        if level not in ParsedConfig.OUTPUT_LEVELS:
-            raise ValueError(f"{level} not a valid output level")
-
-        if file_suffix not in ["rds", "rds_to_h5ad", "h5ad", None]:
-            raise ValueError(f"{file_suffix} not a valid output file suffix")
-
-        file_suffixes = {
-            "prepare": "{method}.h5ad",
-            "integration": "{method}.h5ad",
-            "metrics": "{method}_{o_type}.csv",
-            "embeddings": "{method}_{o_type}.csv",
-            "cc_variance": "{method}_{o_type}.csv",
-            "benchmarks": None
-        }
-
-        # in case of R, we need a different suffix for the integration part
-        if file_suffix == "rds":
-            file_suffixes["integration"] = "R/{method}.RDS"
-        elif file_suffix == "rds_to_h5ad":
-            file_suffixes["integration"] = "R/{method}.h5ad"
-
-        suffix = file_suffixes[file_type]
-
-        if level == "single":
-            return join_path(self.ROOT, "{scenario}", file_type, "{scaling}", "{hvg}", suffix)
-        elif level == "directory_by_setting":
-            return join_path(self.ROOT, "{scenario}", file_type, "{scaling}", "{hvg}")
-        elif level == "by_method":
-            return join_path(self.ROOT, "{{scenario}}", file_type, "{{scaling}}", "{{hvg}}", suffix)
-        elif level == "by_method_scaling":
-            return join_path(self.ROOT, "{{scenario}}", file_type, "{scaling}", "{{hvg}}", suffix)
-        elif level == "final":
-            return join_path(self.ROOT, f"{file_type}.csv")
-        # elif level == "scaled_final":
-        #    return join_path(self.ROOT, f"{file_type}_scaled.csv")
 
     def get_all_python_methods(self):
         return [
@@ -229,79 +180,16 @@ class ParsedConfig:
 
         return comb_func, wildcards
 
-    def get_all_file_patterns(self, file_type, output_types=None):
-        """
-        Collect all expanded file patterns
-        file_type: one of ['integration', 'metrics', 'cc_variance']
-        output_types: output type or list of output types to be considered.
-            If output_types==None, all output types are included.
-            Useful if a certain metric is examined on a specific output type.
-            Output types are ['full', 'embed', 'knn']
-        """
-
-        if file_type not in ParsedConfig.OUTPUT_FILE_TYPES:
-            raise ValueError(f"{file_type} not a valid output file type")
-
-        if output_types is None:
-            output_types = ParsedConfig.OUTPUT_TYPES
+    def get_integrated_for_metrics(self, rules, method):
+        if method == "unintegrated":
+            return Path(rules.integration_prepare.output).with_suffix(".h5ad")
+        elif self.get_from_method(method, "R"):
+            return rules.convert_RDS_h5ad.output
         else:
-            for ot in output_types:
-                if ot not in ParsedConfig.OUTPUT_TYPES:
-                    raise ValueError(f"{output_types} not a valid output type")
+            return rules.integration_run_python.output
 
-        all_files = []
-
-        if file_type == 'metrics_unintegrated':
-            # add unintegrated
-            file_pattern = self.get_filename_pattern("metrics", "single")
-            all_files = expand(file_pattern,
-                               scenario=self.get_all_scenarios(),
-                               hvg="full_feature",
-                               scaling="unscaled",
-                               method="unintegrated", o_type="full")
-        elif file_type == 'embeddings_unintegrated':
-            # add unintegrated
-            file_pattern = self.get_filename_pattern("embeddings", "single")
-            all_files = expand(file_pattern,
-                               scenario=self.get_all_scenarios(),
-                               hvg="full_feature",
-                               scaling="unscaled",
-                               method="unintegrated", o_type="full")
-        else:
-            for method in self.METHODS:
-
-                ot = self.get_from_method(method, "output_type")
-                # keep only types to be considered
-                ot = [x for x in as_list(ot) if x in as_list(output_types)]
-                if not ot:
-                    continue  # skip if method does not have any
-
-                # skip scaling for certain methods
-                scaling = self.SCALING.copy()
-                skip_scaling = self.get_from_method(method, "no_scale")
-                if skip_scaling and "scaled" in scaling:
-                    scaling.remove("scaled")
-
-                file_level = "by_method_scaling"
-
-                if file_type == 'integration':
-                    is_r = self.get_from_method(method, "R")
-                    if is_r:
-                        file_pattern = self.get_filename_pattern(file_type, file_level, "rds_to_h5ad")
-                    else:
-                        file_pattern = self.get_filename_pattern(file_type, file_level, "h5ad")
-
-                    expanded = expand(file_pattern, method=method, scaling=scaling)
-
-                elif file_type in ['metrics', 'cc_variance', 'embeddings']:
-                    file_pattern = self.get_filename_pattern(file_type, file_level)
-                    expanded = expand(file_pattern, method=method, o_type=ot, scaling=scaling)
-                else:
-                    raise ValueError(f"{file_type} is not a valid file type")
-
-                for p in expanded:
-                    f = expand(p, scenario=self.get_all_scenarios(),
-                               hvg=self.get_all_feature_selections())
-                    all_files.extend(f)
-
-        return all_files
+    def get_celltype_option_for_integration(self, wildcards):
+        if self.get_from_method(wildcards.method, "use_celltype"):
+            label_key = self.get_from_scenario(wildcards.scenario, key="label_key")
+            return f"-c {label_key}"
+        return ""
